d7031878339c326c9beba8dcb84c332522936e9c
mahamani484 <134209854+mahamani484@users.noreply.github.com>
2023-05-24 13:33:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+const API_KEY = `7b6046d028bb2c963ad1fd17c1675ee8`
+const form=document.querySelector('form')
+const weather=document.querySelector('#weather')
+const search=document.querySelector('#search')
+// const API = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+// const img_url='https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png'
+
+const getWeather = async(city) => {
+    weather.innerHTML='<h2> LOADING... </h2>'
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=7b6046d028bb2c963ad1fd17c1675ee8&units=metric`
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data)
+    return showWeather(data);
+}
+
+
+const showWeather =(data) =>{
+    if(data.cod=="404"){
+        weather.innerHTML= '<h2> City name not found. <br> Check spelling and try again </h2>'
+        return;
+    }
+    weather.innerHTML= `
+    <div class="weather" id="weather">
+    <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" class="icon" alt="">
+    
+    <h2 class="city"> ${data.main.temp}°C <br>
+     ${data.weather[0].main} </h2>
+    </div>
+`
+}
+
+form.addEventListener("submit",
+function(event){
+    getWeather(search.value);
+    event.preventDefault();
+}
+)
